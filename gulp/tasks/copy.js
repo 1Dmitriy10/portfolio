6ac0596bad3669ed823c7ab57b63921f4c68f4bc
Дмитрий <1primo1@mail.ru>
2023-09-16 13:18:34 +0300

    pub

--- gulp/tasks/copy.js
+++
@@ -1,4 +0,0 @@
-export const copy = () => {
-    return app.gulp.src(app.path.src.files)     //Получаем путь к папке с исходниками из глобального объекта
-        .pipe(app.gulp.dest(app.path.build.files))                  //Указываем куда перенести файлы
-}
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { getMobMenu } from "./burgerMenu.js";
-import { getColorHeader } from "./scroll.js";
-import { getBlueBg } from "./animationBgBlue.js";
-import { certificatesSlider, technologiesSlider } from "./slider.js";
-import { getMinimize } from "./minimizeImg.js";
-import { getAnimation } from "./animationPortfolio.js";

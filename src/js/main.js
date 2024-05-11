4b8332d9cf79b25d089084df819edea0f3e9e9ad
eJwtzLFuwjAQgOF3udkDCSQN3hjaLo0ELarEZJ3tcxrZxO45KaSId6+Ksv7S/90AjaGcj9HTABKq2tbO6GazLbdN4dyqoDU2NRZY6dK45qnUBW7WFQhIHH96S7yst0VS40J1X1Gdvtu6O73nuLta74/uZf/xq1/f7LxrD+3z6nOfDhcQ8FjUOCcCCZqQiUFANvER0J77QaZJh94oT7NgQisjd4IpRTFlYkln7IO4RPYuxH+SrqlnygpHkMMUwv3+B7gQUM4= <1primo1@mail.ru>
2024-05-11 12:33:58 +0300

    Настройка формы обратной связи

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,12 @@ import { anchorScroll } from "./animScroll.js";
 import { activeMenu } from "./activeMenu.js";
 import { tilt } from "./tilt.js";
 import { bg } from "./jquery.buoyant.js";
-import { fancybox } from "./fancybox.js";
+
 import { getAos } from "./aos.js";
+import { inputMask } from "./inputmask.min.js";
+import { validateForm } from "./validateForm.js";
+import { fancybox } from "./fancybox.js";
+
 
 
 
---
+++ src/js/validateForm.js
@@ -0,0 +1,96 @@
+export function validateForm() {
+    $(document).ready(function() {
+        //===маска===
+let phone = $('input[name="tel"]');
+
+let im = new Inputmask("+7 (999) 999-99-99");
+im.mask(phone);
+
+//===валидация формы===
+let count = 1;
+const validator = [];
+$('form').each(function () {
+  $(this).attr('id', `form_${count}`);
+  let id = $(this).attr('id');
+  validator.push(new JustValidate(`#${id}`))
+  count++;
+});
+validator.forEach(el => {
+  el
+    .addField('#basic_name', [
+
+      {
+        rule: 'required',
+        errorMessage: 'Поле не должно быть пустым',
+      },
+      {
+        rule: 'minLength',
+        value: 2,
+        errorMessage: 'Должно быть не менее 2 букв',
+      },
+      {
+        rule: 'maxLength',
+        value: 15,
+        errorMessage: 'Должно быть не более 15 букв',
+      },
+
+    ])
+    .addField('#basic_email', [
+      {
+        rule: 'required',
+        errorMessage: 'Поле не должно быть пустым',
+      },
+      {
+        rule: 'required',
+        errorMessage: 'Не верный формат',
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Не верный формат',
+      },
+    ]);
+  el.onSuccess((event) => {
+    event.preventDefault()
+    formSend(el.form);
+  });
+
+
+})
+
+//====отправка формы====
+async function formSend(el) {
+  let formData = new FormData(el);
+  let response = await fetch('send_mail.php', {
+    method: "POST",
+    body: formData
+  });
+
+
+  //если форма успешно отправлена
+  let messageBox = $('.form-message');
+  let result = await response.json();
+
+
+  if (response.ok) {
+
+    messageBox.innerHTML = '';
+    messageBox.addClass("active_green").append(`<p>${result.message}</p>`);
+    let closeMessage = setTimeout(() => {
+      messageBox.removeClass("active_green").html();
+    }, 5000
+    )
+    el.reset();
+    //если произошла ошибка отпраки
+  } else {
+
+    messageBox.innerHTML = '';
+    messageBox.addClass("active_red").append(`<p>${result.message}</p>`);
+    let closeMessage = setTimeout(() => {
+      messageBox.removeClass("active_green")
+    }, 5000
+    )
+  }
+}
+    })
+};
+validateForm();
